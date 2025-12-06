db95b586f0d0ea959b2d812f4c5e9835d8487cb6
MDuqi <matheusccbr@gmail.com>
2025-12-06 00:56:50 -0300

    feat: initial version utils.py

---
+++ cliente/utils.py
@@ -0,0 +1,20 @@
+#Funções auxiliares do cliente para manipulação de arquivos. (uteis para envio com e sem TLS)
+
+import os
+
+def file_exists(filepath: str) -> bool:
+    #Verifica se o arquivo existe e é um arquivo regular.
+    return os.path.isfile(filepath)
+
+def read_file_chunks(filepath: str, chunk_size: int):
+    #Lê um arquivo em chunks (gerador), evitando carregar tudo na memória.
+    with open(filepath, "rb") as f:
+        while True:
+            data = f.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+def get_filename(filepath: str) -> str:
+    #Extrai somente o nome do arquivo do path.
+    return os.path.basename(filepath)
